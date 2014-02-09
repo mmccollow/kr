7df22143894c20847e6b0000094bfea19f8dcffd
Matt <mmccollow@gmail.com>
2014-02-08 23:24:50 -0500

    Initial commit

---
+++ 1-4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* print Celsius-Fahrenheit table
+		for fahr = 0, 29, ..., 300; floating-point version */
+int main() {
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	celsius = lower;
+	printf("Celsius Fahrenheit \n");
+	while (celsius <= upper) {
+		fahr = (celsius / (5.0/9.0)) + 32.0;
+		printf("%3.0f %8.1f\n", celsius, fahr);
+		celsius = celsius + step;
+	}
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Solutions to exercises from The C Programming Language, 2nd Edition by Kernighan and Ritchie
+
+To build an exercise: gcc <exercise#>.c -o <output_file_name>
+
